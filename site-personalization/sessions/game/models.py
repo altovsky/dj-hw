f93faf73e570e3dda76bab69ab24a87223b8038d
altovsky <igor.altovsky@yandex.ru>
2019-04-28 00:17:57 +0300

    site-personalization ++

--- site-personalization/sessions/game/models.py
+++ site-personalization/sessions/game/models.py
@@ -20,20 +20,7 @@ class Game(models.Model):
         2 - окончена
 
     """
-    # identifier = models.IntegerField(verbose_name='Идентификатор игры')
-    # creator = models.ForeignKey(
-    #     Player,
-    #     on_delete=models.CASCADE,
-    #     verbose_name='Создатель игры'
-    # )
-    # opponent = models.ForeignKey(
-    #     Player,
-    #     related_name='creators',
-    #     on_delete=models.CASCADE,
-    #     verbose_name='Второй игрок'
-    # )
     number = models.IntegerField(verbose_name='Число')
-
     game_state = models.IntegerField(default=0, verbose_name='Код состояния')
 
     class Meta:
@@ -45,21 +32,10 @@ class Game(models.Model):
 
 
 class PlayerGameInfo(models.Model):
-    # creators = models.ManyToManyField(Player)  #, related_name='creators')
-    # opponents = models.ManyToManyField(Player)  #, related_name='opponents')
-    play = models.ForeignKey(Game, on_delete=models.CASCADE)  #, related_name='games')
-    # games = models.ManyToManyField(Game)  #, related_name='games')
-    # creator = models.CharField(max_length=96, verbose_name='Создатель игры')
-    # number = models.IntegerField(max_length=10, verbose_name='Число')
+    play = models.ForeignKey(Game, on_delete=models.CASCADE)
     player_attempts = models.IntegerField(verbose_name='Число попыток', default=0)
-    # second_player = models.ForeignKey(
-    #     Player,
-    #     default=0,
-    #     on_delete=models.CASCADE
-    # )
     second_player = models.ManyToManyField(
         Player,
-        # default=0,
     )
 
     class Meta:
